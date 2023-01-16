c13c597dff5537d365bbdcee64c331e9220fc5dc
Danielateresa <dany.t_87@hotmail.it>
2023-01-16 15:22:44 +0100

    Define relationship methods Type and Project mod.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -16,5 +17,15 @@ class Project extends Model
     {
         $project_slug = Str::slug($title);
         return $project_slug;
+    }
+
+     /**
+     * Get all of the posts for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function types(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);//un progetto può appartenere a una categoria
     }
 }
